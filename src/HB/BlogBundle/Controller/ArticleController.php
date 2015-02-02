7ef141e30645179255ec348b3d0c21abc1a61912
HumanBooster <bclerc@humanbooster.com>
2015-02-02 11:44:21 +0100

    ajout des noms de route, actions edit et delete

--- src/HB/BlogBundle/Controller/ArticleController.php
+++ src/HB/BlogBundle/Controller/ArticleController.php
@@ -20,7 +20,7 @@ class ArticleController extends Controller
     /**
      * Liste tous les articles
      * 
-     * @Route("/")
+     * @Route("/", name="article_list")
      * @Template()
      */
     public function indexAction()
@@ -39,42 +39,12 @@ class ArticleController extends Controller
     /**
      * Affiche un formulaire pour ajouter un article
      *
-     * @Route("/add")
+     * @Route("/add", name="article_add")
      * @Template()
      */
     public function addAction()
     {
-    	$article = new Article();
-    	
-    	// on créé un objet formulaire en lui précisant quel Type utiliser
-    	$form = $this->createForm(new ArticleType, $article);
-    	
-    	// On récupère la requête
-    	$request = $this->get('request');
-    	
-    	// On vérifie qu'elle est de type POST pour voir si un formulaire a été soumis
-    	if ($request->getMethod() == 'POST') {
-    		// On fait le lien Requête <-> Formulaire
-    		// À partir de maintenant, la variable $article contient les valeurs entrées dans
-    		// le formulaire par le visiteur
-    		$form->bind($request);
-    		// On vérifie que les valeurs entrées sont correctes
-    		// (Nous verrons la validation des objets en détail dans le prochain chapitre)
-    		if ($form->isValid()) {
-    			// On l'enregistre notre objet $article dans la base de données
-    			$em = $this->getDoctrine()->getManager();
-    			$em->persist($article);
-    			$em->flush();
-    	
-    			// On redirige vers la page de visualisation de l'article nouvellement créé
-    			return $this->redirect(
-    				$this->generateUrl('article_read', array('id' => $article->getId()))
-    					);
-    		}
-    	}
-    	
-    	// passe la vue de formulaire à la vue
-    	return array( 'formulaire' => $form->createView() );	
+		return $this->editAction(0);
     }
     
     /**
@@ -99,22 +69,74 @@ class ArticleController extends Controller
 	/**
 	 * Affiche un formulaire pour éditer un article sur un Id
 	 *
-	 * @Route("/{id}/edit")
-	 * @Template()
+	 * @Route("/{id}/edit", name="article_edit")
+	 * @Template("HBBlogBundle:Article:add.html.twig")
 	 */
 	public function editAction($id)
 	{
-		return array();
+		if ($id>0) {
+			// on récupère le repository de l'Article
+			$repository = $this->getDoctrine()->getRepository("HBBlogBundle:Article");
+			// on demande au repository l'article par l'id
+			$article = $repository->find($id);
+		} else {
+			// on créé un nouvel article (on vient de l'add)
+			$article = new Article();
+		}
+		
+		$article = new Article();
+		 
+		// on créé un objet formulaire en lui précisant quel Type utiliser
+		$form = $this->createForm(new ArticleType, $article);
+		 
+		// On récupère la requête
+		$request = $this->get('request');
+		 
+		// On vérifie qu'elle est de type POST pour voir si un formulaire a été soumis
+		if ($request->getMethod() == 'POST') {
+			// On fait le lien Requête <-> Formulaire
+			// À partir de maintenant, la variable $article contient les valeurs entrées dans
+			// le formulaire par le visiteur
+			$form->bind($request);
+			// On vérifie que les valeurs entrées sont correctes
+			// (Nous verrons la validation des objets en détail dans le prochain chapitre)
+			if ($form->isValid()) {
+				// On l'enregistre notre objet $article dans la base de données
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($article);
+				$em->flush();
+				 
+				// On redirige vers la page de visualisation de l'article nouvellement créé
+				return $this->redirect(
+						$this->generateUrl('article_read', array('id' => $article->getId()))
+				);
+			}
+		}
+		 
+		// passe la vue de formulaire à la vue
+		return array( 'formulaire' => $form->createView() );
 	}
 	
 	/**
 	 * Supprime un article sur un Id
 	 *
-	 * @Route("/{id}/delete")
-	 * @Template()
+	 * @Route("/{id}/delete", name="article_delete")
 	 */
 	public function deleteAction($id)
 	{
-		return array();
+		// on récupère le repository de l'Article
+		$em = $this->getDoctrine()->getManager();
+		
+		// on demande au repository l'article par l'id
+		$article = $em->getRepository("HBBlogBundle:Article")->find($id);
+		
+		// on demande à l'entity manager de supprimer l'article
+		$em->remove($article);
+		$em->flush();
+	
+		// On redirige vers la page de liste des articles
+		return $this->redirect(
+				$this->generateUrl('article_list')
+		);
 	}
 }
